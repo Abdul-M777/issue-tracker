78291e90635e0dc440d3f37f4381a037569cea63
Abdulmuaz Alshaikhli <slmnshukur1993@gmail.com>
2025-04-10 07:35:51 +0200

    Handling delete errors

--- app/issues/[id]/DeleteIssueButton.tsx
+++ app/issues/[id]/DeleteIssueButton.tsx
@@ -1,11 +1,23 @@
 "use client";
 import { AlertDialog, Button, Flex } from "@radix-ui/themes";
 import axios from "axios";
+import { on } from "events";
 import { useRouter } from "next/navigation";
-import React from "react";
+import React, { useState } from "react";
 
 const DeleteIssueButton = ({ issueId }: { issueId: number }) => {
   const router = useRouter();
+  const [error, setError] = useState(false);
+
+  const onDelete = async () => {
+    try {
+      await axios.delete("/api/issues/" + issueId);
+      router.push("/issues");
+      router.refresh();
+    } catch (error) {
+      setError(true);
+    }
+  };
 
   return (
     <>
@@ -26,17 +38,25 @@ const DeleteIssueButton = ({ issueId }: { issueId: number }) => {
               </Button>
             </AlertDialog.Cancel>
             <AlertDialog.Action>
-              <Button
-                color="red"
-                onClick={async () => {
-                  await axios.delete("/api/issues/" + issueId);
-                  router.push("/issues");
-                  router.refresh();
-                }}
-              >
+              <Button color="red" onClick={onDelete}>
                 Delete Issue
               </Button>
             </AlertDialog.Action>
+          </Flex>
+        </AlertDialog.Content>
+      </AlertDialog.Root>
+      <AlertDialog.Root open={error} onOpenChange={setError}>
+        <AlertDialog.Content>
+          <AlertDialog.Title>Deletion Failed</AlertDialog.Title>
+          <AlertDialog.Description>
+            An error occurred while deleting the issue. Please try again later.
+          </AlertDialog.Description>
+          <Flex mt="4" gap="3">
+            <AlertDialog.Cancel>
+              <Button variant="soft" color="gray">
+                Close
+              </Button>
+            </AlertDialog.Cancel>
           </Flex>
         </AlertDialog.Content>
       </AlertDialog.Root>
